49f028d09f07f9f8729b56c14b4006ec03c93f88
NickR123 <Nickrandall5@gmail.com>
2019-02-07 14:09:00 +0000

    Add project files.

---
+++ ProjectTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("885ab41d-47c3-4ff9-875d-0cf39157ce10")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProjectTesting/tstBuzzTronic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectTesting
+{
+    [TestClass]
+    public class tstBuzzTronic
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
