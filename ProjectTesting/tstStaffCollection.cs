f5d06d1b65b25d21d546c305458c24c8facde8b7
NickR123 <Nickrandall5@gmail.com>
2019-03-12 12:54:56 +0000

    Started collection class

---
+++ ProjectTesting/tstStaffCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectClassLibrary;
+using System.Collections.Generic;
+
+namespace ProjectTesting
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+       [TestMethod]
+       public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //test that it exists
+            Assert.IsNotNull(AllStaff);
+        }
+
+    }
+}
