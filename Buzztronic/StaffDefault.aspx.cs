465ab09b94b1dadcd21bdb6317ac22f57786cb82
NickR123 <Nickrandall5@gmail.com>
2019-03-12 16:04:47 +0000

    Added presentation layer

---
+++ Buzztronic/StaffDefault.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ProjectClassLibrary;
+
+namespace Buzztronic
+{
+    public partial class StaffDefault : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
