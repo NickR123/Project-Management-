5bc25a667cd27ad2eea18dca6cc595a28c9bcccd
NickR123 <Nickrandall5@gmail.com>
2019-03-13 17:14:39 +0000

    Started presentation layer work

--- ProjectClassLibrary/clsStaffCollection.cs
+++ ProjectClassLibrary/clsStaffCollection.cs
@@ -1,6 +1,56 @@
-﻿namespace ProjectClassLibrary
+﻿using System;
+using System.Collections.Generic;
+namespace ProjectClassLibrary
 {
     public class clsStaffCollection
     {
+        List<clsStaff> mStaffList = new List<clsStaff>();
+
+        public clsStaffCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblStaff_SelectAll");
+            RecordCount = DB.Count;
+            while(Index < RecordCount)
+            {
+                clsStaff AnStaff = new clsStaff();
+                AnStaff.StaffNo = Convert.ToInt32(DB.DataTable.Rows[Index]["StaffNo"]);
+                AnStaff.FirstName = Convert.ToString(DB.DataTable.Rows[Index]["FirstName"]);
+                AnStaff.LastName = Convert.ToString(DB.DataTable.Rows[Index]["LastName"]);
+                AnStaff.StaffAddress = Convert.ToString(DB.DataTable.Rows[Index]["Address"]);
+                AnStaff.StaffPhoneNo = Convert.ToInt32(DB.DataTable.Rows[Index]["PhoneNo"]);
+                AnStaff.StaffPostCode = Convert.ToString(DB.DataTable.Rows[Index]["PostCode"]);
+                mStaffList.Add(AnStaff);
+                Index++;
+            }
+        }
+
+        public List<clsStaff> StaffList
+        {
+            get
+            {
+                return mStaffList;
+            }
+            set
+            {
+                mStaffList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+     
+                //return the count of the list
+                return mStaffList.Count;
+            }
+            set
+            {
+                // we shall worry about this later
+            }
+        }
+        public clsStaff ThisStaff { get; set; }
     }
 }
--- ProjectTesting/tstStaffCollection.cs
+++ ProjectTesting/tstStaffCollection.cs
@@ -17,5 +17,74 @@ namespace ProjectTesting
             Assert.IsNotNull(AllStaff);
         }
 
+        [TestMethod]
+        public void StaffListOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            List<clsStaff> TestList = new List<clsStaff>();
+
+            clsStaff TestItem = new clsStaff();
+            //set its properties
+            TestItem.StaffNo = 1;
+            TestItem.FirstName = "John";
+            TestItem.LastName = "Smith";
+            TestItem.StaffAddress = "Some Address";
+            TestItem.StaffPhoneNo = 1234567891;
+            TestItem.StaffPostCode = "LE7 6AB";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllStaff.StaffList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStaff.StaffList, TestList);
+        }
+
+
+        [TestMethod]
+        public void ThisStaffPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //create some test data to assign to the property
+            clsStaff TestStaff = new clsStaff();
+            //set the properties of the test object
+            TestStaff.StaffNo = 1;
+            TestStaff.FirstName = "John";
+            TestStaff.LastName = "Smith";
+            TestStaff.StaffAddress = "Some Address";
+            TestStaff.StaffPhoneNo = 1234567891;
+            TestStaff.StaffPostCode = "LE7 6AB";
+            //assign the data to the property
+            AllStaff.ThisStaff = TestStaff;
+            Assert.AreEqual(AllStaff.ThisStaff, TestStaff);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            List<clsStaff> TestList = new List<clsStaff>();
+
+            clsStaff TestItem = new clsStaff();
+            //set its properties
+            TestItem.StaffNo = 1;
+            TestItem.FirstName = "John";
+            TestItem.LastName = "Smith";
+            TestItem.StaffAddress = "Some Address";
+            TestItem.StaffPhoneNo = 1234567891;
+            TestItem.StaffPostCode = "LE7 6AB";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllStaff.StaffList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStaff.Count, TestList.Count);
+        }
+
+
+
+
     }
 }
