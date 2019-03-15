3aee03b0d9ded973bfe5eb060ea4f198a23f0b12
Hamza Iqbal <P17177241@my365.dmu.ac.uk>
2019-03-15 23:43:15 +0000

    create product collection class

---
+++ ProjectClassLibrary/clsProductCollection.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ProjectClassLibrary
+{
+    public class clsProductCollection
+    {
+        List<clsProduct> mProductList = new List<clsProduct>();
+        clsProduct mThisProduct = new clsProduct();
+
+        public clsProductCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblProduct_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsProduct AnProduct = new clsProduct();
+                AnProduct.ProductNo = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductNo"]);
+                AnProduct.ProductName = Convert.ToString(DB.DataTable.Rows[Index]["ProductName"]);
+                AnProduct.Description = Convert.ToString(DB.DataTable.Rows[Index]["Description"]);
+                AnProduct.Price = Convert.ToDecimal(DB.DataTable.Rows[Index]["Price"]);
+                AnProduct.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                
+                mProductList.Add(AnProduct);
+                Index++;
+            }
+        }
+
+        public List<clsProduct> ProductList
+        {
+            get
+            {
+                return mProductList;
+            }
+            set
+            {
+                mProductList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+
+                //return the count of the list
+                return mProductList.Count;
+            }
+            set
+            {
+                // we shall worry about this later
+            }
+        }
+        public clsProduct ThisProduct
+        {
+            get
+            {
+                //return the private data
+                return mThisProduct;
+            }
+            set
+            {
+                //set the private data
+                mThisProduct = value;
+            }
+        }
+
+        public int Add()
+        {
+            //adds a new record to the database based on the valyes of mThisProduct
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters
+            DB.AddParameter("@ProductName", mThisProduct.ProductName);
+            DB.AddParameter("@Description", mThisProduct.Description);
+            DB.AddParameter("@Price", mThisProduct.Price);
+            DB.AddParameter("@Active", mThisProduct.Active);
+            
+            //execute quey return the primary key value
+            return DB.Execute("sproc_tblProduct_Insert");
+        }
+
+        public void Delete()
+        {
+            //deletes the record pointed to by thisproduct
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@ProductNo", mThisProduct.ProductNo);
+            //execute the stored procedure
+            DB.Execute("sproc_tblProduct_Delete");
+        }
+    }
+}
---
+++ ProjectTesting/tstProductCollection.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectClassLibrary;
+
+namespace ProjectTesting
+{
+    [TestClass]
+    public class tstProductCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsProductCollection AllProducts = new clsProductCollection();
+            //test that it exists
+            Assert.IsNotNull(AllProducts);
+        }
+        [TestMethod]
+        public void ProductListOK()
+        {
+            //create an instance of the class we want to create
+            clsProductCollection AllProducts = new clsProductCollection();
+            List<clsProduct> TestList = new List<clsProduct>();
+
+            clsProduct TestItem = new clsProduct();
+            //set its properties
+            TestItem.ProductNo = 1;
+            TestItem.ProductName = "Samsung";
+            TestItem.Description = "Black";
+            TestItem.Price = 1299;
+            TestItem.Active = true;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllProducts.ProductList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllProducts.ProductList, TestList);
+        }
+        [TestMethod]
+        public void ThisProductPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProductCollection AllProducts = new clsProductCollection();
+            //create some test data to assign to the property
+            clsProduct TestProduct = new clsProduct();
+            //set the properties of the test object
+            TestProduct.ProductNo = 1;
+            TestProduct.ProductName = "Samsung";
+            TestProduct.Description = "Black";
+            TestProduct.Price = 1299;
+            TestProduct.Active = true;
+            //assign the data to the property
+            AllProducts.ThisProduct = TestProduct;
+            Assert.AreEqual(AllProducts.ThisProduct, TestProduct);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsProductCollection AllProducts = new clsProductCollection();
+            List<clsProduct> TestList = new List<clsProduct>();
+
+            clsProduct TestItem = new clsProduct();
+            //set its properties
+            TestItem.ProductNo = 1;
+            TestItem.ProductName = "Samsung";
+            TestItem.Description = "Black";
+            TestItem.Price = 1299;
+            TestItem.Active = true;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllProducts.ProductList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllProducts.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsProductCollection AllProducts = new clsProductCollection();
+            //create the item of test data
+            clsProduct TestItem = new clsProduct();
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.ProductNo = 1;
+            TestItem.ProductName = "Samsung";
+            TestItem.Description = "Black";
+            TestItem.Price = 1299;
+            TestItem.Active = true;
+            //set ThisProduct to test the data
+            AllProducts.ThisProduct = TestItem;
+            //add the record
+            PrimaryKey = AllProducts.Add();
+            TestItem.ProductNo = PrimaryKey;
+            //find the record 
+            AllProducts.ThisProduct.Find(PrimaryKey);
+            //test to see that the two values are the same
+            Assert.AreEqual(AllProducts.ThisProduct, TestItem);
+
+        }
+
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsProductCollection AllProducts = new clsProductCollection();
+            //create the item of test data
+            clsProduct TestItem = new clsProduct();
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.ProductNo = 1;
+            TestItem.ProductName = "Samsung";
+            TestItem.Description = "Black";
+            TestItem.Price = 1299;
+            TestItem.Active = true;
+            //set ThisProduct to test the data
+            AllProducts.ThisProduct = TestItem;
+            //add the record
+            PrimaryKey = AllProducts.Add();
+            TestItem.ProductNo = PrimaryKey;
+            //find the record 
+            AllProducts.ThisProduct.Find(PrimaryKey);
+            //delete the record
+            AllProducts.Delete();
+            //now find the record
+            Boolean Found = AllProducts.ThisProduct.Find(PrimaryKey);
+            //test to see that the record was not found
+            Assert.IsFalse(Found);
+
+
+        }
+    }
+}
